6ab4e094a1f379e93d68160ba7d80dd77041d97c
Thay <thaywane.oliveira@gmail.com>
2024-03-21 17:05:11 -0300

    correção de visualização do react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header/Header";
 import * as S from "./components/Header/header_styled";
 import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
+
 
 export default function App() {
 return (
     <>
     <S.GlobalStyle/>
     <Header/>
-    <h2>VNW</h2>
     <Main/>
     <Footer/>
     </>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,1 +1,9 @@
 import * as S from "./footer_styled"
+
+import React from 'react'
+
+export default function Footer() {
+return (
+    <div>Footer</div>
+)
+}
